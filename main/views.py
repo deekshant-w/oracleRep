5acab52288f771f064a249871e37c6195a8f7ab9
Deekshant Wadhwa <46841177+deekshant-w@users.noreply.github.com>
2021-03-23 19:58:41 +0530

    Frontend connected to backend

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 
 def landing(request):
+    if(request.method=="POST"):
+        print(request.POST)
+        return JsonResponse({'status':"ok", "next":"/"})
     return render(request,"landing.html")
--- static/js/landing.js
+++ static/js/landing.js
@@ -1,12 +1,61 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function showLoader(){
+    Swal.fire({
+        title: "Processing your request ...<br><br>",
+        html:
+        `<div class="spinner-border text-primary" role="status"><span class="visually-hidden">Loading...</span></div>`,
+        showConfirmButton: false,
+        allowOutsideClick: false
+      })
+}
+
+function showError(err="There was an error in processing your request!", rea='Oops..'){
+    Swal.fire({
+        icon: 'error',
+        title: rea,
+        text: err,
+    })
+}
+
 var getReport = ()=>{
-    let data = {};
+    showLoader();
+    let data = new FormData();
+    let csrftoken = getCookie('csrftoken');
+
     let subVal = document.getElementById("reportSelect").value;
     if(subVal==""){
-        // AJAX request example
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Please Select a Ledgeer',
-        })
+        showError('Please Select a Ledgeer');
+        return
     }
+    data.append('subVal',subVal)
+    fetch('/',{
+        method: "POST",
+        body: data,
+        headers: { "X-CSRFToken": csrftoken },
+    })
+    .then(response=>response.json())
+    .then(res=>{
+        if(res.status=='ok'){
+            window.location = res.next;
+        }
+        else{
+            throw "Request Failed: "+String(res)
+        }
+    })
+    .catch(err=>showError(err))
 }
