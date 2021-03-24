574ae061af364479ee73c09c2844b9229cff609f
Niteshks247 <niteshks247@gmail.com>
2021-03-24 15:30:32 +0530

    halfwaythrough

--- main/views.py
+++ main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from oracleRep.settings import connection
 
 def landing(request):
     if(request.method=="POST"):
         print(request.POST)
+        cursor = connection.cursor()
+        res = cursor.execute("""
+        select column_name
+        from user_tab_cols
+        where table_name = 'BANKDATA'
+    """)
+        for i in res:
+            print(i)
         return JsonResponse({'status':"ok", "next":"/"})
     return render(request,"landing.html")
